1824b56adcdf10dba6e3eb1f41520b66bbf0966e
SeaCrap <506076395@qq.com>
2018-10-10 21:04:59 +0800

    点击扣钱，刷新会恢复

---
+++ server.js
@@ -0,0 +1,45 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+var port = process.env.PORT || 8888; //bash设置端口：PORT=8001 node server.js
+
+var server = http.createServer(function(request, response){
+  
+  var temp = url.parse(request.url,true)
+  var path = temp.pathname
+  var query = temp.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+
+    if(path === '/'){
+    let string = fs.readFileSync('./index.html','utf8')
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
+    response.end()
+  }else if(path === '/style.css'){
+    let string = fs.readFileSync('./style.css','utf8')
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/css')
+    response.write(string)
+    response.end()
+  }else if(path === '/main.js'){
+    let string = fs.readFileSync('./main.js','utf8')
+    response.setHeader('Content-Type', 'application/javascript;charset=utf-8')
+    response.write(string)
+    response.end()
+  }else{
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('呜呜呜')
+    response.end()
+  }
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
